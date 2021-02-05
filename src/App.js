d2164fd2d773513c3bd782b18fe85dbabb4df3aa
Chaz Carbis <chazcarbis@gmail.com>
2021-02-05 14:58:57 -0600

    reupload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import { Route, Link, Switch, } from 'react-router-dom'
+import PizzaForm from './PizzaForm'
 
 const App = () => {
+
+  const initialFormValues = {
+    name: '',
+    size: '',
+    cheese: false,
+    pepperoni: false,
+    sausage: false,
+    ham: false,
+    instructions: '',
+  }
+
+
+
+  const [formValues, setFormValues] = useState(initialFormValues)
+  
+  const inputChange = (name, value) => {
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const formSubmit = () => {
+    const newPizza = {
+      name: formValues.name.trim(),
+      size: formValues.size,
+      toppings: formValues.toppings,
+      instructions: formValues.instructions.trim(),
+    }
+  }
+
   return (
-    <>
+    <div>
+    <nav>
+      <div>
+        <Link to= '/'>Home</Link>
+        <Link to= '/pizza'>Place an order</Link>
+      </div>
+    </nav>
       <h1>Lambda Eats</h1>
-    </>
+
+      <Switch>
+        <Route exact path= '/pizza'>
+          <PizzaForm values= {formValues} change= {inputChange} submit= {formSubmit}/>
+        </Route>
+      </Switch>
+    </div>
   );
 };
 export default App;
